f108e40801e9e6e465fd89f295b5ca0ef3154fc7
lt <longtaotao1314@gmail.com>
2019-07-08 17:34:47 +0800

    es ikmapping

--- src/main/java/com/longtao/test/ESTesst.java
+++ src/main/java/com/longtao/test/ESTesst.java
@@ -1,5 +1,7 @@
 package com.longtao.test;
 
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingAction;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.get.MultiGetItemResponse;
@@ -9,6 +11,7 @@ import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -26,6 +29,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -227,7 +231,7 @@ public class ESTesst {
     }
 
     /**
-     * 查询多少对象
+     * 查询多少个对象
      */
     @Test
     public void matchAllQuery(){
@@ -240,11 +244,142 @@ public class ESTesst {
         System.out.println("查询结果有：" + hits.getTotalHits() + "条");
 
         for (SearchHit hit: hits){
-            System.out.println(hit.getSourceAsString());
+            System.out.println("结果：" + hit.getSourceAsString());
         }
         client.close();
 
     }
 
+    /**
+     * 条件查询
+     */
+    @Test
+    public void query(){
+        // condition search
+        SearchResponse searchResponse = client.prepareSearch("blog")
+                .setTypes("article")
+                .setQuery(QueryBuilders.queryStringQuery("大数据")).get();
+        // 打印
+        SearchHits hits = searchResponse.getHits();
+        System.out.println("查询到的结果：" + hits.getTotalHits() + "条");
+        for (SearchHit hit: hits){
+            System.out.println("结果：" + hit);
+        }
+        // 关闭
+        client.close();
+    }
+
+    /**
+     * 通配符查询
+     */
+    @Test
+    public void wildcardQuery(){
+        // 通配符查询
+        SearchResponse searchResponse = client.prepareSearch("blog")
+                .setTypes("article")
+                .setQuery(QueryBuilders.wildcardQuery("content","*全*")).get();
+        // 获取命中次数 %大数据%
+        SearchHits hits = searchResponse.getHits();
+        System.out.println("查询到的结果：" + hits.getTotalHits() + "条");
+        for (SearchHit hit: hits){
+            System.out.println("结果：" + hit);
+        }
+        // 关闭
+        client.close();
+    }
+
+    /**
+     * field查询
+     */
+    @Test
+    public void termQuery(){
+        // 类似于mysql =
+        SearchResponse searchResponse = client.prepareSearch("blog")
+                .setTypes("article")
+                .setQuery(QueryBuilders.termQuery("content","全文")).get();
+        SearchHits hits = searchResponse.getHits();
+        System.out.println("查询到的结果：" + hits.getTotalHits() + "条");
+        for (SearchHit hit: hits){
+            System.out.println("结果：" + hit);
+        }
+        // 关闭
+        client.close();
+    }
+
+    /**
+     * 模糊查询
+     */
+    @Test
+    public void fuzzy(){
+        // 模糊查询
+        SearchResponse searchResponse = client.prepareSearch("blog")
+                .setTypes("article")
+                .setQuery(QueryBuilders.fuzzyQuery("title","lucence")).get();
+        SearchHits hits = searchResponse.getHits();
+        System.out.println("查询到的结果：" + hits.getTotalHits() + "条");
+        Iterator<SearchHit> iterator = hits.iterator();
+
+        while (iterator.hasNext()){
+            SearchHit searchHit = iterator.next();// 每个查询对象
+            System.out.println("结果：" + searchHit);
+        }
+
+        // 关闭
+        client.close();
+    }
+
+    /**
+     * 添加mapping
+     */
+    @Test
+    public void createMapping() throws Exception {
+        // 设置mapping
+        XContentBuilder builder = XContentFactory.jsonBuilder().startObject().startObject("article")
+                .startObject("properties").startObject("id1").field("type","text").field("store","true").endObject()
+                .startObject("titile2").field("type","text").field("store","false").endObject()
+                .startObject("content").field("type","text").field("store","true").endObject().endObject().endObject().endObject();
+
+        // 添加mapping
+        PutMappingRequest mapping = Requests.putMappingRequest("blog4").type("article").source(builder);
+
+        client.admin().indices().putMapping(mapping).get();
+        client.close();
+    }
+
+    @Test
+    public void createMappingIk() throws Exception {
+        // 1设置mapping
+        XContentBuilder builder = XContentFactory.jsonBuilder().startObject().startObject("article")
+                .startObject("properties").startObject("id1").field("type", "text").field("store", "true")
+                .field("analyzer", "ik_smart").endObject().startObject("title2").field("type", "text")
+                .field("store", "false").field("analyzer", "ik_smart").endObject().startObject("content")
+                .field("type", "text").field("store", "true").field("analyzer", "ik_smart").endObject().endObject()
+                .endObject().endObject();
+
+        // 添加mapping
+        PutMappingRequest mapping = Requests.putMappingRequest("blog4").type("article").source(builder);
+
+        client.admin().indices().putMapping(mapping).get();
+        client.close();
+    }
+
+    /**
+     * 词条查询
+     */
+    @Test
+    public void queryTermForik(){
+        // 分析结果 默认standard
+        // analyzer分词器分词，大写字母全部转为了小写字母，并存入了倒排索引以供搜索。term是确切查询， 必须要匹配到大写的Name。
+        SearchResponse response = client.prepareSearch("blog4").setTypes("article")
+                .setQuery(QueryBuilders.termQuery("content","web")).get();
+        // 获取结果
+        SearchHits hits = response.getHits();
+
+        System.out.println("结果条数：" + hits.getTotalHits());
+        // 循环打印
+        for (SearchHit hit : hits){
+            System.out.println("结果：" + hit.getSourceAsString());
+        }
+    }
 
 }
